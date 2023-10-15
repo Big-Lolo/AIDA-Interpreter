5a884df7f38dbb383f917950c37ba55f286d4d8e
Big_Lolo <95545807+Big-Lolo@users.noreply.github.com>
2023-10-15 02:32:32 +0200

    Primeros cambios
    Creacion de patrones de respuestas de alarmas:
    - algo menos cuarto, media, 20...
    - Algo en punto
    - Algo algo
    - Algo y Algo
    ....

--- src/incorporations/functions.py
+++ src/incorporations/functions.py
@@ -1,21 +1,74 @@
-from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
-import pandas as pd
-import torch
-
-from ...src.main import tokenizer, model
+import spacy
+import re
+nlp = spacy.load("es_core_news_sm")  ##Interprete de oracion con tal de sacar argumentos de las oraciones
 
 
-##Funciones
 
-def generar_respuesta(instruccion):
-    # Tokenizar la instrucción
-    inputs = tokenizer(instruccion, return_tensors="pt")
 
-    # Obtener la salida del modelo
-    outputs = model(**inputs)
+def extract_alarm_information(text):
+    doc = nlp(text)
+    menos_x_pattern = re.compile(r'(\d+) menos (\d+)')
+    en_punto_pattern = re.compile(r'(\d+) en punto')
+    patron_con_y = re.compile(r'las (\d+) y (\d+)')
+    patron_sin_separacion = re.compile(r'a las (\d+) (\d+)')
+    time_pattern = r'(\d+)\s*([yY])\s*media'  # Buscar patrón de "n y media"
+    match_en_punto = en_punto_pattern.search(text)
+    match_con_y = patron_con_y.search(text)
+    match_sin_separacion = patron_sin_separacion.search(text)
+    match_time_pattern = re.search(time_pattern, text)
+
+
+    alarm_info = {
+        "hora": [0, 0], #horas, minutos
+        "fecha": None,
+        "iteratividad": None,
+        "vibracion": None,
+        "volumen": None
+    }
+    word_to_minute = {
+        'media': 30,
+        'cuarto': 15
+    }
+    if match_en_punto:
+        print("b")
+        alarm_info["hora"][1] = 0
+        alarm_info["hora"][0] = match_en_punto.group(1)
     
-    # Aquí puedes procesar la salida según tu necesidad
-    # En este ejemplo, simplemente decodificamos la salida
-    respuesta = tokenizer.decode(outputs.logits.argmax(), skip_special_tokens=True)
+    if match_con_y:
+        print("a")
+        alarm_info["hora"][0] = int(match_con_y.group(1))
+        alarm_info["hora"][1] = int(match_con_y.group(2))
+    elif match_sin_separacion:
+        print("c")
+        alarm_info["hora"][0] = int(match_sin_separacion.group(1))
+        alarm_info["hora"][1] = int(match_sin_separacion.group(2))
+
+    for token in doc:
+        if token.text.lower() in ["mañana", "tarde", "noche"]:
+            alarm_info["hora"][0] = int(token.text)
+        
+        if token.text.lower() in word_to_minute:
+            alarm_info["hora"][1] += int(word_to_minute[token.text.lower()])
+        if token.text.lower() == 'menos':
+            match = menos_x_pattern.search(text)
+            if match:
+                if alarm_info["hora"][1] == 0:
+                    alarm_info["hora"][1] =60 - int(match.group(1))
+                    
+                else:
+                    alarm_info["hora"][1] -= int(match.group(2))
+                    
+                alarm_info["hora"][0] = (int(match.group(1)) - 1)
+
+
+    return alarm_info
+
+
+async def set_alarm(knowledge_base):
+    #Creando alarma...
+    user_input: str = input(f'{knowledge_base["systemInfo"]["user_name"]} Config Alarma: ')
+
+    infoAlarm =  extract_alarm_information(user_input)
+    print(infoAlarm)
+    return infoAlarm
     
-    return respuesta
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import json
 from difflib import get_close_matches
+from incorporations.functions import extract_alarm_information, set_alarm  ##Funcion para sacar el argumento de tiempode una oración.
+import asyncio
 
 
 def load_knowledge_base(file_path:str) -> dict:
@@ -20,13 +22,13 @@ def find_best_match(user_question: str, questions: list[str]) -> str| None:
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
-            return q["answer"]
+            return q["answer"], q["function"]
         
-def chat_bot():
+async def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
     while True:
-        user_input: str = input('You: ')
+        user_input: str = input(f'{knowledge_base["systemInfo"]["user_name"]}: ')
 
         if user_input.lower() == 'quit':
             break
@@ -34,16 +36,22 @@ def chat_bot():
         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
 
         if best_match:
-            answer: str = get_answer_for_question(best_match, knowledge_base)
-            print(f'Bot: {answer}')
+            answer, function = get_answer_for_question(best_match, knowledge_base)
+            print(f'{knowledge_base["systemInfo"]["name_bot"]}: {answer}')
+            await eval(function + f"({knowledge_base})")
+             
         else:
-            print('Bot: I don\'t know the answer. Can you teach me=?')
+            print(f'{knowledge_base["systemInfo"]["name_bot"]}: I don\'t know the answer. Can you teach me=?')
             new_answer: str = input('Type the answer or "skip" to skip: ')
 
             if new_answer.lower() != 'skip':
                 knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
                 save_knowledge_base('knowledge_base.json', knowledge_base)
-                print('Bot: Thank you! I learned a new response!')
+                print(f'{knowledge_base["systemInfo"]["name_bot"]}: Thank you! I learned a new response!')
+
+
+
+
 
 if __name__ == '__main__':
-    chat_bot()
+    asyncio.run(chat_bot())
